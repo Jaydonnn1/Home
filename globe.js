79567dbf8efc7ff02843d091f35c5d37fd80d88a
JoshHaydonRowe <93529741+Jaydonnn1@users.noreply.github.com>
2025-01-23 21:14:48 +0000

    Update globe.js

--- globe.js
+++ globe.js
@@ -2,7 +2,7 @@ class Globe {
     constructor() {
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0x000000);
-        
+    
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.set(0, 30, 100);
         
